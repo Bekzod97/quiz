ecae8b76aab409b5e3f09c1e75db524b343b04ad
Bekzod97 <bekzod.mirzayev97@mail.ru>
2023-10-25 13:37:55 +0500

    finished html/css

---
+++ js/data.js
@@ -0,0 +1,26 @@
+const data = {
+    title: "Название опроса",
+    questions: [
+        {
+            question: "Вопрос - 1",
+            answers: ["ответ - 1", "ответ - 2", "ответ - 3", "ответ - 4"],
+            validAnswer: 2
+        },
+        {
+            question: "Вопрос - 2",
+            answers: ["ответ - 1", "ответ - 2", "ответ - 3", "ответ - 4"],
+            validAnswer: 1
+        },
+        {
+            question: "Вопрос - 3",
+            answers: ["ответ - 1", "ответ - 2", "ответ - 4"],
+            validAnswer: 3
+        },
+
+        {
+            question: "Вопрос - 3",
+            answers: ["ответ - 1", "ответ - 2", "ответ - 4"],
+            validAnswer: 3
+        },
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const dom = {
+    title: document.getElementById("title"),
+    progress: {
+        progressfill: document.getElementById("progress-fill"),
+        questionNumber: document.getElementById("question-number"),
+        totalQuestion: document.getElementById("total-question")
+    },
+
+    step: {
+        question: document.getElementById("question"),
+        questionPosition: document.getElementById("question-position"),
+    },
+
+    answers: document.getElementById("answers"),
+
+}
+
+let questionCount = data.questions.length;
+let step = 1;
+
+function renderProgress({
+    total,
+    step, 
+    progressfillElem,
+    
+}){
+    
+    dom.
+}
+
+console.log(questionCount);
+console.log(dom);    
